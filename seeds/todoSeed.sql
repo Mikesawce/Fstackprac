90581fb57c3941275fcae3715e3ddbde3f37fa20
Michael Mote <michael.mote1993@gmail.com>
2023-11-21 13:52:55 -0600

    added basics

---
+++ seeds/todoSeed.sql
@@ -0,0 +1,3 @@
+INSERT INTO todo (task) VALUES ('bake a cake');
+INSERT INTO todo (task) VALUES ('slap a map');
+INSERT INTO todo (task) VALUES ('choke a bloke');
---
+++ tables/todo.sql
@@ -0,0 +1,5 @@
+CREATE TABLE todo (
+    id SERIAL PRIMARY KEY,
+    task VARCHAR(255) NOT NULL,
+    complete BOOLEAN DEFAULT FALSE
+);
